3b61d4e187695715c45267ca2d80aa7a81919cf3
Ajeet Kumar Maurya <ajeetkumar99190@gmail.com>
2018-08-21 15:09:44 +0530

    Update College.java

--- src/college/College.java
+++ src/college/College.java
@@ -721,7 +721,7 @@ public class College extends javax.swing.JFrame {
         Session session = Session.getDefaultInstance(props,
                 new javax.mail.Authenticator() {
                 protected PasswordAuthentication getPasswordAuthentication(){
-                return new PasswordAuthentication("ajeetkumar99190@gmail.com","9598725268");
+                return new PasswordAuthentication("ajeetkumar99190@gmail.com","password");
                 }        
                 }  
                 );
